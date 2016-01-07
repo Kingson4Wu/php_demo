461301c5d34fd4935bb088bde1b90d1a39063c46
Kingson4Wu <Kingson_Wu@163.com>
2016-01-07 22:57:07 +0800

    Create utils.php

---
+++ utils.php
@@ -0,0 +1,11 @@
+//1.echo输出乱码：
+<?php
+header('Content-type: text/html; charset=UTF8'); // UTF8不行改成GBK试试，与你保存的格式匹配
+?>
+
+//2.使用PHP的cURL库可以简单和有效地去抓网页
+<http://coolshell.cn/articles/664.html>
+
+ <http://coolshell.cn/articles/11.html>
+九个PHP很有用的功能
+<http://coolshell.cn/articles/2394.html>
